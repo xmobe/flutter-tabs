7a239e26b8cd935c0563a256ef2b959c193ca661
Quang Anh <quanganh@aiti.com.vn>
2018-08-28 14:24:52 +0700

    Tabs Base

---
+++ lib/pages/flutter.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FlutterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Flutter Page content here'),
+      ),
+    );
+  }
+}
